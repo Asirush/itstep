6b509bcbd4505a3045313f611216a0c6130f9e6a
Абдухаликов Асир Александрович <@itstep.loc>
2021-11-16 21:26:44 +0600

    first_lesson_cout

--- lesson1/main.cpp
+++ lesson1/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!";
+using namespace std;
+
+int main()
+{
+//    cout<<"Hello World\n" << "2+2\n" << 2+2 << endl << endl << "Asir";
+    cout << "\"Asir\"\n 20"<< endl << endl;
+
+    cout <<"      /"<< endl; 
+    cout <<"     /"<< endl; 
+    cout <<"    /"<< endl; 
+    cout <<"   /"<< endl; 
+    cout <<"  /"<< endl;
+    cout <<" /"<< endl << endl; 
+   
+    cout <<"      /\n     /\n    /\n   /\n  /\n /\n"<<" \\\n  \\\n   \\\n    \\\n     \\\n      \\\n" << endl << endl;
+    cout <<"      / \\\n     /   \\\n    /     \\\n   /       \\\n  /         \\\n /           \\\n"<< endl << endl;
+    
+    cout << "Asir" << "\t" << "hello" << endl << endl;      //adding horizontal tabulation - 4 spaces
+    cout << "Asir" << "\a" << endl << endl;                 // makes sound 
+   
+   
+ 
+   
     return 0;
 }
