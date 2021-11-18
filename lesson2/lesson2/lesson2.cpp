cf6c088adfe07903d0b7132a5fcff940c932efeb
АбдухаликовА <asirushtomblack@gmail.com>
2021-11-18 19:14:51 +0600

    lesson2 R2

--- lesson2/lesson2/lesson2.cpp
+++ lesson2/lesson2/lesson2.cpp
@@ -5,7 +5,13 @@ using namespace std;
 
 int main()
 {
-	cout << R"(\t\n)" << endl;
+//	cout << R"(\t\n)" << endl; // how to output \t \n or any of this, if you need to output )", user R"x()x"
+
+	
+	
+	
+	
+	
 	return 0;
 }
 
