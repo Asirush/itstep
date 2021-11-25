509c7f89bffacf38f1fe82a9e064959b44b4768f
АбдухаликовА <asirushtomblack@gmail.com>
2021-11-25 19:20:20 +0600

    lesson 4, 1st task

--- lesson4/lesson4.cpp
+++ lesson4/lesson4.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
@@ -7,6 +6,21 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int end;
+//первая задача
+
+	int x, y, z;
+	cout << "Введите количество ягод собранных Машей: "; cin >> x; cout << endl;
+	cout << "Введите количество ягод собранных Мишей: "; cin >> y; cout << endl;
+	cout << "Введите количество ягод сьеденных ребятами: "; cin >> z; cout << endl;
+
+	int a = x + y;
+
+	if (a >= z) {
+		cout << "ребята собрали: " << x + y - z << endl;
+	}
+	else {
+		cout << "ребята не могли сьесть больше собранного!";
+	}
 
 	cin >> end;
 	return 0;
