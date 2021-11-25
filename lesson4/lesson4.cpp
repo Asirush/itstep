29d76e0e421957877a39999974dd6dbce6b725f7
АбдухаликовА <asirushtomblack@gmail.com>
2021-11-25 20:53:20 +0600

    lesson4 task2 updated

--- lesson4/lesson4.cpp
+++ lesson4/lesson4.cpp
@@ -23,17 +23,25 @@ int main()
 	}
 */
 //вторая задача
-	int m1, m2, m3, havyest;
+	int m1, m2, m3, havyest, havyest2, counter=1;
 	cout << "Введите массу первого толстяка: "; cin >> m1; cout << endl;
 	cout << "Введите массу второго толстяка: "; cin >> m2; cout << endl;
 	cout << "Введите массу третьего толстяка: "; cin >> m3; cout << endl;
 
 	if ((m1 < 94 || m1>727) || (m2 < 94 || m2>727) || (m3 < 94 || m3>727)) { cout << "указана некорректная масса толстяка!" << endl; }
+
+	if ((m1 == m2) && (m1 > m3)) { havyest = m1; havyest2 = m2; counter += 1; }
+	if ((m2 == m3) && (m2 > m1)) { havyest = m2; havyest2 = m3; counter += 1; }
+	if ((m1 == m3) && (m1 > m2)) { havyest = m1; havyest2 = m3; counter += 1; }
+
 	else if (m1 > m2 && m1 > m3) { havyest = m1; }
 	else if (m2 > m1 && m2 > m3) { havyest = m2; }
 	else if (m3 > m1 && m3 > m2) { havyest = m3; }
-	cout << "самый тяжелый толстяк: " << havyest << endl;
 
+	if (counter == 1) { cout << "победитель: " << havyest << endl; }
+	else {
+		cout << "победитель: " << havyest << " " << havyest2 << endl;
+	}
 	cin >> end;
 	return 0;
 }
