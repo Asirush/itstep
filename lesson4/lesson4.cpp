c83b8dd60d02141bb915b4bdf3a680ed7d7de535
АбдухаликовА <asirushtomblack@gmail.com>
2021-11-25 19:55:31 +0600

    lesson 4 task 2

--- lesson4/lesson4.cpp
+++ lesson4/lesson4.cpp
@@ -7,7 +7,7 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	int end;
 //первая задача
-
+/*
 	int x, y, z;
 	cout << "Введите количество ягод собранных Машей: "; cin >> x; cout << endl;
 	cout << "Введите количество ягод собранных Мишей: "; cin >> y; cout << endl;
@@ -21,6 +21,18 @@ int main()
 	else {
 		cout << "ребята не могли сьесть больше собранного!";
 	}
+*/
+//вторая задача
+	int m1, m2, m3, havyest;
+	cout << "Введите массу первого толстяка: "; cin >> m1; cout << endl;
+	cout << "Введите массу второго толстяка: "; cin >> m2; cout << endl;
+	cout << "Введите массу третьего толстяка: "; cin >> m3; cout << endl;
+
+	if ((m1 < 94 || m1>727) || (m2 < 94 || m2>727) || (m3 < 94 || m3>727)) { cout << "указана некорректная масса толстяка!" << endl; }
+	else if (m1 > m2 && m1 > m3) { havyest = m1; }
+	else if (m2 > m1 && m2 > m3) { havyest = m2; }
+	else if (m3 > m1 && m3 > m2) { havyest = m3; }
+	cout << "самый тяжелый толстяк: " << havyest << endl;
 
 	cin >> end;
 	return 0;
