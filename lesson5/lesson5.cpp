53e7a493fc302f5d78116a2aa44764b5c1fe082f
АбдухаликовА <asirushtomblack@gmail.com>
2021-11-30 21:46:40 +0600

    lesson5 task2 done

--- lesson5/lesson5.cpp
+++ lesson5/lesson5.cpp
@@ -31,7 +31,36 @@ int main()
 	if ((n1 + n2 + n3) == (n4 + n5 + n6)) { cout << "Поздравляем! Ваш билет счастливый!" << endl; }
 	else { cout << "Сегодня Вам не повезло(((" << endl; }
 	*/
+	//Неявное и явное преобразование переменных
+	/*
+	* неявное 
+	* float a = 1.35;
+	* int b = a;
+	* 
+	* явное 
+	* int number = 97;
+	* char symbol;
+	* symbol = (char)number;
+	* 
+	* float c = static_cast<int>(a); //making a from float to int*/
 	//task2
+
+	int hh, mm, b, c, hh_out, mm_out;
+	cout << "Введите время начала пути: "; cin >> hh>>mm; cout << endl;
+	cout << "Введите количество часов в пути: ";  cin >> b; cout << endl;
+	cout << "Введите количество минут в пути: ";  cin >> c; cout << endl;
+	if ((mm + c) >= 60) 
+	{
+		hh += 1; mm_out = (mm + c) % 60;
+	 if ((hh + b) >= 24) { hh_out = (hh + b) % 24; }
+	 else { hh_out = hh + b; }
+	}
+	else {
+		mm_out = mm + c;
+		if ((hh + b) >= 24) { hh_out = (hh + b) % 24; }
+		else { hh_out = hh + b; }
+	}
+	cout << "Время по приезде: " << hh_out << ":" << mm_out << endl;
 
 
 	cin >> end;
