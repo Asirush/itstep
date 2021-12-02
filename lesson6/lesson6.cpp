0bba00b0cfaf35a69a13d3a406d647c9c6c7f3b7
АбдухаликовА <asirushtomblack@gmail.com>
2021-12-02 20:21:30 +0600

    lesson 6 task 2

--- lesson6/lesson6.cpp
+++ lesson6/lesson6.cpp
@@ -6,24 +6,45 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int end;
-	
-	int hh, mm, b, c, hh_out, mm_out, days;
+//task 1
+	/*
+	int hh, mm, hh_in, mm_in, hh_out, mm_out, days, last_hh, last_mm;
 	cout << "Введите время начала пути: "; cin >> hh >> mm; cout << endl;
-	cout << "Введите количество часов в пути: ";  cin >> b; cout << endl;
-	cout << "Введите количество минут в пути: ";  cin >> c; cout << endl;
-	if ((mm + c) >= 60)
+	cout << "Введите количество часов и минут в пути: ";  cin >> hh_in >> mm_in; cout << endl;
+
+	//last_hh = hh_out % 24; last_mm = mm_out % 60;
+
+	if ((mm + mm_in) >= 60)
 	{
-		hh += 1; mm_out = (mm + c) % 60;
-		if ((hh + b) >= 24) { hh_out = (hh + b) % 24; days = (hh + b)/24; }
-		else { hh_out = hh + b; days = (hh + b) / 24;}
+		hh += 1; mm_out = (mm + mm_in) % 60;
+		if ((hh + hh_in) >= 24) { hh_out = (hh + hh_in) % 24; days = (hh + hh_in)/24; last_hh = hh_out % 24; last_mm = mm_out % 60;
+		}
+		else { hh_out = hh + hh_in; days = (hh + hh_in) / 24; last_hh = hh_out % 24; last_mm = mm_out % 60;
+		}
 	}
 	else {
-		mm_out = mm + c;
-		if ((hh + b) >= 24) { hh_out = (hh + b) % 24; days = (hh + b) / 24;}
-		else { hh_out = hh + b; days = (hh + b) / 24;}
+		mm_out = mm + mm_in;
+		if ((hh + hh_in) >= 24) { hh_out = (hh + hh_in) % 24; days = (hh + hh_in) / 24; last_hh = hh_out % 24; last_mm = mm_out % 60;
+		}
+		else { hh_out = hh + hh_in; days = (hh + hh_in) / 24; last_hh = hh_out % 24; last_mm = mm_out % 60;
+		}
 	}
 	cout << "Время по приезде: " << hh_out << ":" << mm_out << endl;
-	cout << "Суток прошло: " << days << endl;
+	cout << "Суток прошло: " << days << endl << "Остаточных часов и минут: "<< last_hh << ":"<< last_mm<<endl;
+	*/
+//100 20 50
+//task 2
+	int N, i, j, out;
+	cout << "Введите первое количество станций в ветке: "; cin >> N; cout << endl;
+	cout << "Введите номер станции посадки Вити: "; cin >> i; cout << endl;
+	cout << "Введите Номер станции выхода Вити: "; cin >> j; cout << endl;
+
+	if (i < j && ((j - i) <= N / 2)) { out = j-i; }
+	if (i > j && ((i - j) <= N / 2)) { out = i-j; }
+	if (i < j && ((j - i) > N / 2)) { out = N - j + i; }
+	if (i > j && ((i - j) > N / 2)) { out = N - i + j; }
+
+	cout << "Количество станций равно: " << out << endl;
 
 	cin >> end;
 	return 0;
