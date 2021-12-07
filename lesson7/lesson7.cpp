90afd6bc523285a9d8c4d9bbe337bd3650cec199
АбдухаликовА <asirushtomblack@gmail.com>
2021-12-07 19:25:13 +0600

    lesson 7 cycles

--- lesson7/lesson7.cpp
+++ lesson7/lesson7.cpp
@@ -7,6 +7,12 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	int end;
 
+	for (size_t i = 0; i < 100; i++)
+	{
+		//для дебага используй "точки останова"(ПКМ - точка останова) 
+		cout << i << endl;
+	}
+
 	cin >> end;
 	return 0;
 }
