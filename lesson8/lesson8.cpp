fa9f9c1710968a449f6817260f49a6050ba917aa
АбдухаликовА <asirushtomblack@gmail.com>
2021-12-09 19:31:08 +0600

    lesson 8 task1

--- lesson8/lesson8.cpp
+++ lesson8/lesson8.cpp
@@ -5,10 +5,28 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int end;
+
+	int N, flag, j = 0;
+//task 1
+    cout << "Enter number of rows: ";
+    cin >> N;
+
+    for (int i = 1, k = 0; i <= N; ++i, k = 0)
+    {
+        for (flag = 1; flag <= N - i; ++flag)
+        {
+            cout <<"  ";
+        }
+
+        while (k != 2 * i - 1)
+        {
+            cout << "* ";
+            ++k;
+        }
+        cout << endl;
+    }
 
 
-
-	cin >> end;
+	cout << endl;
 	return 0;
 }
