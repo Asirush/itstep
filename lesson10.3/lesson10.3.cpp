b807db44f40657f13a8cc0d496c5856b32676631
АбдухаликовА <asirushtomblack@gmail.com>
2021-12-14 19:40:38 +0600

    lesson 10 task 1

---
+++ lesson10.3/lesson10.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int x, y, days = 0;
+	cin >> x>>y;
+	
+	while (x < y) {
+		x = x+x*0.1;
+		days++;
+	}
+
+	cout << "days: " << days << endl;
+	Sleep(1000);
+
+
+	return 0;
+}
