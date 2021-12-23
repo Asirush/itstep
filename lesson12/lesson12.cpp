312e1290547d11da7bb3b08012809451731c3ddf
АбдухаликовА <asirushtomblack@gmail.com>
2021-12-23 20:07:26 +0600

    lesson 12 task 1.3 + homework

--- lesson12/lesson12.cpp
+++ lesson12/lesson12.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	//task1 
-
+	/*
 	int N, i=1, temp, count_of_warm_days=0, coldest_day, counter=1;
 	cin >> N;
 
@@ -30,7 +30,43 @@ int main()
 		}
 		i++;
 	}
-	cout << endl << "min_temp: " << coldest_day << endl << "warm days count: " << count_of_warm_days << endl;
+	cout << endl << "min_temp: " << coldest_day << endl << "warm days count: " << count_of_warm_days << endl << "cold days: " << N - count_of_warm_days << endl;;
+	*/
+	//homework - task1
+	/*
+	for (int i = 100; i <= 999; i++) {
+		if (i/100 == (i % 10) || i/100 == (i%100 - i%10) || i%10 == (i%100 - i%10)) {
+			cout <<endl<< i;
+		}
+	}
+	cout << endl;
+	*/
+	//homework - task 2
+	/*
+	int counter = 899;
+	for (int i = 100; i <= 999; i++) {
+		if (i / 100 == (i % 10) || i / 100 == (i % 100 - i % 10) || i % 10 == (i % 100 - i % 10)) {
+			counter--;
+		}
+	}
+	cout << endl << counter << endl;;
+	*/
+	//homework - task 3
+	/*
+	int r = 0, N;
+	cin >> N;
+
+		while (N != 0) {
+			r = N % 10;
+			N = N / 10;
+			if (r == 3 || r == 6) {
+				
+			}
+			else {
+				cout << r;
+			}
+		}
+	*/
 
 
 
