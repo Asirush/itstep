a0ca80498117522da728ebaf3ef595d204ec4fd0
АбдухаликовА <asirushtomblack@gmail.com>
2021-12-23 19:33:36 +0600

    lesson 12 task 1.1

--- lesson12/lesson12.cpp
+++ lesson12/lesson12.cpp
@@ -6,37 +6,32 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	//task1 
-/*
-	int N, i=1, temp, count_of_warm_days=0, coldest_day;
+
+	int N, i=1, temp, count_of_warm_days=0, coldest_day, counter=1;
 	cin >> N;
 
 	while (i<=N)
 	{
 		cin >> temp;
-		if (temp>0)
+		if (temp > 0)
 		{
 			count_of_warm_days++;
-			if (i == 1)
+			if (i == counter)
 			{
 				coldest_day = temp;
 			}
+			else if (temp < coldest_day) {
+				coldest_day = temp;
+			}
+			
 		}
-		else
-		{
-			if (i==1)
-			{
-				coldest_day = temp;
-			}
-			else {
-				if (temp < coldest_day) {
-					coldest_day = temp;
-				}
-			}
+		else {
+			counter++;
 		}
 		i++;
 	}
 	cout << endl << "min_temp: " << coldest_day << endl << "warm days count: " << count_of_warm_days << endl;
-*/
+
 
 
 	return 0;
