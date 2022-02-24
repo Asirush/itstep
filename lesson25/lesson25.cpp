c90079458d8686f1d16da5e9a94ed7d84cc8b027
АбдухаликовА <asirushtomblack@gmail.com>
2022-02-24 18:56:34 +0600

    lesson 25 start

---
+++ lesson25/lesson25.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+/*
+int main()
+{
+    //указатели
+    int a = 24;
+    int* pa = &a;
+
+    cout << "a = " << a << endl << "p address: " << pa << endl << "p meaning: " << *pa << endl;
+
+}*/
+
