ee9d83723b0a2afc789021f50420360846da3841
АбдухаликовА <asirushtomblack@gmail.com>
2022-03-03 21:03:37 +0600

    динамическое выделение данных

--- lesson27/lesson27.cpp
+++ lesson27/lesson27.cpp
@@ -47,11 +47,46 @@ void swap2(int& a, int& b) {
 	b = temp;
 }
 
+void todouble(int *a) {
+	*a *= 2;
+}
+
+void todouble2(int& a) {
+	a *= 2;
+}
+
 int main() {
-	int a = 5;
+	/*int a = 5;
 	int b = 10;
 	cout << a << " " << b << endl;
 	swap(&a, &b);
 	cout << a << " " << b << endl;
-	return 0;
+	return 0;*/
+
+	// создание динамического массива
+	int n;
+	cin >> n;
+
+	int* parray = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		parray[i] = rand() % 5;
+		cout << parray[i] << " ";
+	}
+	cout << endl;
+	// очистка динамических данных
+	//delete []parray; //delete parray;
+
+	if (parray!=0) { //if(parray==0)
+		delete[]parray; //delete parray;
+		parray = nullptr;
+		cout << "array has been deleted" << endl;
+	}
+	else { cout << "parray has been deleted" << endl; }
+	//for (int i = 0; i < n; i++)
+	//{
+	//	cout << parray[i] << endl;
+	//}
+
 }
