d1d36057a5d1f2e924d8b6df0d4f86f208a3ee4f
Assir Abdukhalikov <asirushtomblack@gmail.com>
2022-04-18 19:24:09 +0600

    lesson 2, object init

---
+++ lesson2.2/lesson2.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+class MyClass
+{
+public:
+	MyClass();
+	MyClass(int i);
+	//explicit MyClass(int i);    //запрет, на неявный вызов метода
+	MyClass(double i);
+
+private:
+	int test;
+};
+
+int main()
+{
+
+	// variables format is the same class
+	int test(0);
+	MyClass test_2 = 0;
+	//if you wish to use create object ONLY this way add "explicit MyClass(int i);"
+	MyClass test_3(0);
+}
+
+MyClass::MyClass()
+{
+	test = 0;
+}
+
+MyClass::MyClass(int i)
+{
+	test = i;
+}
+
+MyClass::MyClass(double i)
+{
+	test = (int)i;
+}
