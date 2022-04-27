3287f51d3dddeb19ce669f0772b2d900778f078b
Assir Abdukhalikov <asirushtomblack@gmail.com>
2022-04-27 19:20:11 +0600

    example int

---
+++ cpp_associate/lesson2.5/lesson2.5.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+class my_int {
+public:
+	my_int();
+	my_int(const int v);
+	my_int(const double v);
+	operator float();
+	my_int operator+(const my_int& obj);
+
+private:
+	int value;
+};
+
+int main()
+{
+	int a = 5;
+	int b = a;
+	a = b + 5;
+
+	my_int c;
+	my_int d = 5;
+	my_int e = 5.0; // неявное приведение типа
+	my_int f = (float)5.0; // явное приведение типа
+
+}
+
+my_int::my_int() : value(0)
+{
+
+}
+
+my_int::my_int(const int v) : my_int()
+{
+	this->value = v;
+}
+
+my_int::my_int(const double v)
+{
+	this->value = (int)v;
+}
+
+my_int::operator float()
+{
+	return (float)this->value;
+}
+
+my_int my_int::operator+(const my_int& obj)
+{
+	return my_int(this->value + obj.value);
+}
+
+
