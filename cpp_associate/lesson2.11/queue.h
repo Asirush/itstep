73c56147a37209b417647ea3d91e9a71e95f0def
Assir Abdukhalikov <asirushtomblack@gmail.com>
2022-05-23 21:00:12 +0600

    lesson 12

---
+++ cpp_associate/lesson2.11/queue.h
@@ -0,0 +1,100 @@
+﻿#pragma once
+#include <iostream>
+
+template<typename type>
+class queue {
+private:
+	type* wait;			//Очередь
+	int maxLength;		//Максимальный размер очереди
+	int length;			//Текущий размер очереди
+	int l, r;           //индекс первого и последнего элемента очереди
+public:
+	queue();			//Конструктор
+	queue(int m);		//Конструктор с размером очереди
+	void add(type c);	//Добавление элемента    
+	type extract();		//Извлечение элемента
+	void clear();		//Очистка очереди
+	bool is_empty();	//Проверка существования элементов в очереди
+	bool is_full();		//Проверка на переполнение очереди
+	int get_count();	//Количество элементов в очереди
+	void show();		//демонстрация очереди
+	~queue();			//Деструктор                       
+};
+
+template<typename type>
+inline queue<type>::queue()
+{
+	new type mass[1];
+	wait = *mass[0];
+	maxLength = 1;
+	l = 0, r = 0;
+}
+
+template<typename type>
+inline queue<type>::queue(int m)
+{
+	wait = type* mass[m];
+	maxLength = m;
+	l = 0, r = 0;
+}
+
+template<typename type>
+inline void queue<type>::add(type c)
+{
+	wait[r] = c;
+	r++;
+}
+
+template<typename type>
+inline type queue<type>::extract()
+{
+	type a = wait[l];
+	l++;
+	return a;
+}
+
+template<typename type>
+inline void queue<type>::clear()
+{
+	l = 0; r = 0;
+}
+
+template<typename type>
+inline bool queue<type>::is_empty()
+{
+	if (l == r && r != maxLength) return true;
+	else return false;
+}
+
+template<typename type>
+inline bool queue<type>::is_full()
+{
+	if (l == r && r == maxLength) return true;
+	else return false;
+}
+
+template<typename type>
+inline int queue<type>::get_count()
+{
+	int count = 0;
+	for (auto i = l; i <= r; i++) {
+		count++;
+	}
+	return count;
+}
+
+template<typename type>
+inline void queue<type>::show()
+{
+	for (auto i = l; i <= r; i++) {
+		std::cout << i << ": " << wait[i] << std::endl;
+	}
+}
+
+template<typename type>
+inline queue<type>::~queue()
+{
+	delete[] wait;
+	delete[] maxLength;
+	delete[] l, r, length;
+}
---
+++ cpp_associate/lesson2.11/stack.h
@@ -0,0 +1,118 @@
+﻿#pragma once
+#include <iostream>
+
+template<typename type>
+class stack {
+	type* data;				// Массив для хранения данных
+	int _top;				// Указатель на вершину стека
+public:
+	stack();					// Конструктор по умолчанию
+	stack(int size);			// Конструктор для инициализации размера стека
+	stack(const stack& st);		// Конструктор копирования
+	stack(stack&& st);			// Конструктор переноса
+
+	bool empty()const;			// Пустой стек?						
+	void push(type& val);		// Добавление элемента в конец стека
+	void push(type&& val);		// Перенос элемента в конец стека
+	int size()const;			// Количество элементов в стеке
+	void pop();					// Удаление последнего элемента в стеке
+	const type& top()const;		// Возврат последнего элемента в стеке
+	void swap(stack& val);		// Обмен 2х стеков местами
+
+	~stack();					// Деструктор
+};
+
+template<typename type>
+inline stack<type>::stack()
+{
+	data = new template<typename type>[1];
+}
+
+template<typename type>
+inline stack<type>::stack(int size)
+{
+	data = new template<typename type>[size];
+}
+
+template<typename type>
+inline stack<type>::stack(const stack& st)
+{
+	size_t data_size = st.data.size();
+	for (size_t i = 0; i < data_size; i++) {
+		data[i] = st.data[i];
+	}
+}
+
+template<typename type>
+inline stack<type>::stack(stack&& st)
+{
+	data = st.data;
+	delete[] st;
+}
+
+template<typename type>
+inline bool stack<type>::empty() const
+{
+	if (_top) return true;
+	else return false;
+}
+
+template<typename type>
+inline void stack<type>::push(type& val)
+{
+	data[_top++] = val;
+	_top++;
+}
+
+template<typename type>
+inline void stack<type>::push(type&& val)
+{
+	for (size_t i = 0; i < _top; i++) {
+		if (data[i] == val) {
+			template<typename type> mem = data[i];
+			int flag = i;
+			while (flag != _top--) {
+				data[flag] = data[flag++];
+				flag++;
+			}
+			data[_top] = mem;
+		}
+	}
+}
+
+template<typename type>
+inline int stack<type>::size() const
+{
+	int i = 0, answer;
+	while (data[i]) {
+		answer++;
+	}
+	return answer;
+}
+
+template<typename type>
+inline void stack<type>::pop()
+{
+	_top--;
+}
+
+template<typename type>
+inline const type& stack<type>::top() const
+{
+	_top++;
+}
+
+template<typename type>
+inline void stack<type>::swap(stack& val)
+{
+	type* mem = data;
+	data = val.data;
+	val.data = mem;
+}
+
+template<typename type>
+inline stack<type>::~stack()
+{
+	delete[] data;
+	delete[] _top;
+}
