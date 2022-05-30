2a40cb25bcb0fc5f57ca236bb5ed6c23f9aa8583
Assir Abdukhalikov <asirushtomblack@gmail.com>
2022-05-30 21:16:01 +0600

    end of the lesson 14

--- cpp_associate/lesson2.13/SEP-212.1/SEP-212.1/group.cpp
+++ cpp_associate/lesson2.13/SEP-212.1/SEP-212.1/group.cpp
@@ -2,23 +2,42 @@
 
 void group::add(student bk)
 {
-	group.push_back(bk);
+	students.push_back(bk);
 }
 
 void group::remove(student bk)
 {
-	auto size = group.size();
+	auto size = students.size();
 	std::vector<student> vs;
 	for (auto i = 0; i < size; i++)
-		if (!group[i].equal(bk))
-			vs.push_back(group[i]);
-	group = vs;
+		if (!students[i].equal(bk))
+			vs.push_back(students[i]);
+	students = vs;
 }
 
-std::ostream& operator<<(std::ostream& os, const group shlf)
+void group::add_coach(coach bk)
 {
-	auto size = shlf.group_num.size();
+	group_coach.push_back(bk);
+}
+
+void group::remove_coach(coach bk)
+{
+	auto size = group_coach.size();
+	std::vector<coach> vs;
 	for (auto i = 0; i < size; i++)
-		os << shlf.group[i] << std::endl;
+		if (!group_coach[i].equal(bk))
+			vs.push_back(group_coach[i]);
+	group_coach = vs;
+}
+
+std::ostream& operator<<(std::ostream& os, const student shlf)
+{
+	auto size = shlf.coach.size();
+	for (auto i = 0; i < size; i++)
+		os << shlf.coach[i] << std::endl;
+
+	auto size = shlf.students.size();
+	for (auto i = 0; i < size; i++)
+		os << shlf.students[i] << std::endl;
 	return os;
 }
--- cpp_associate/lesson2.13/SEP-212.1/SEP-212.1/group.h
+++ cpp_associate/lesson2.13/SEP-212.1/SEP-212.1/group.h
@@ -2,13 +2,17 @@
 #include<iostream>
 #include<vector>
 #include"student.h"
+#include"coach.h"
 
 class group
 {
-	std::vector<student> group;
+	std::vector<student> students;
+	std::vector<coach> group_coach;
 public:
 	void add(student bk);
 	void remove(student bk);
+	void add_coach(coach bk);
+	void remove_coach(coach bk);
 	friend std::ostream& operator<<(std::ostream& os, const student shlf);
 };
 
