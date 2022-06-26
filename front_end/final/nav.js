9389fc110804f6cea241497009fb7389f3e6b8c1
Assir Abdukhalikov <asirushtomblack@gmail.com>
2022-06-26 22:18:03 +0600

    end of front_end

---
+++ front_end/final/nav.js
@@ -0,0 +1,34 @@
+$(function(){
+	function Anton()
+	{
+		$('#home').toggleClass('none');
+		$('#home').removeClass('anim');
+	}
+	
+	function changeBlockNone()
+	{
+		$('#home').toggleClass('anim');
+	}
+	
+	/*function Misha()
+	{
+		$('#b').toggleClass('block');
+	}*/
+	
+	$('#burgm').on('click', function(){		
+		Anton();	
+		
+	});
+	
+	/*$('#logo').on('click', function(){		
+		Misha();	
+		
+	});*/
+	
+	$('#closem').on('click', function(){		
+		changeBlockNone();	
+		setTimeout(Anton, 1000);	
+		
+	});
+		
+});
