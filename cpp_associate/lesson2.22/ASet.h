048bdc1e943947ca5da5a08c1165d43903ad6482
Assir Abdukhalikov <asirushtomblack@gmail.com>
2022-07-04 20:49:04 +0600

    finish task

--- cpp_associate/lesson2.22/ASet.h
+++ cpp_associate/lesson2.22/ASet.h
@@ -1,6 +1,4 @@
 ﻿#pragma once
-#include <list>
-#include <array>
 
 class ASet
 {
--- cpp_associate/lesson2.22/lesson2.22.cpp
+++ cpp_associate/lesson2.22/lesson2.22.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "ASet.h"
 
 int main() {
+	ASet crap(3, 8);
+	crap.incl(10);
+	crap.print();
+
 
 	return 0;
 }
