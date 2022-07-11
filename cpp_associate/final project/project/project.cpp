ad9d65120d709073f44197602f6ec6a523ac7dbf
Assir Abdukhalikov <asirushtomblack@gmail.com>
2022-07-11 18:54:02 +0600

    lesson 23 and final project

---
+++ cpp_associate/final project/project/project.cpp
@@ -0,0 +1,21 @@
+/*
+https://gist.github.com/be9/bad0196a86175ea95028
+https://github.com/be9/oopcode/tree/master/wgs_84
+https://www.gpslib.ru/tracks/info/50467/Gonka-IV-Etap-velokavkaza.html
+*/
+
+#include <iostream>
+#include "Geopoint.h"
+
+using namespace std;
+
+int main() {
+
+    Geopoint nsk_lenin_square(55.030039, 82.920084, 15),
+        msk_red_square(55.753915, 37.620525, 15);
+
+    cout << "Distance (km) = " <<
+        nsk_lenin_square.distanceTo(msk_red_square) / 1000 << endl;
+
+    return 0;
+}
