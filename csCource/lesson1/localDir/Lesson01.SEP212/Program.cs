672f45da956ec56caebfc8cb5cb423d6a9db2320
asirush <asirushtomblack@gmail.com>
2022-08-15 20:42:53 +0600

    edited directories

---
+++ csCource/lesson1/localDir/Lesson01.SEP212/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01.SEP212
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Asir";
+
+            Console.Write("введите ФИО: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("приветствую тебя" + userName + "!");
+
+            Console.Write("введите возраст: ");
+            string userAge = Console.ReadLine();
+
+            int currentAge = 2022;
+            userAge = Convert.ToInt32(userAge);
+
+            int avalAge = currentAge - userAge;
+
+            if (avalAge >= 18)
+            {
+                Console.WriteLine("приветствую тебя" + userName + "!");
+            }
+            else
+            {
+                Console.WriteLine("слишком молод");
+            }
+        }
+    }
+}
