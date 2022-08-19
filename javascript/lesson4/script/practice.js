06b237239b7b99e3c20c23e82945d1010badc8cd
asirush <asirushtomblack@gmail.com>
2022-08-19 20:15:58 +0600

    task 3 done

--- javascript/lesson4/script/practice.js
+++ javascript/lesson4/script/practice.js
@@ -18,3 +18,19 @@ function pow(num1, num2){
 console.log(pow(2,4));
 
 // task 3
+function calc(num1, sign, num2){
+    switch (sign){
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case '/':
+            return num1/num2;
+        case '*':
+            return num1*num2;
+    }
+}
+
+console.log(calc(2, "+", 5));
+
+// task 4
