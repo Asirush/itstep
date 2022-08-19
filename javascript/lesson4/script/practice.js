c05928d6ad110241cb5e386e2014c1d9833040d3
asirush <asirushtomblack@gmail.com>
2022-08-19 20:09:14 +0600

    practice

---
+++ javascript/lesson4/script/practice.js
@@ -0,0 +1,20 @@
+// task 1
+function min(num1, num2){
+
+    if(num1 < num2){console.log(num1)}
+    else if(num1 > num2){console.log(num2)}
+    else{console.log("values are equal")}
+    //return num1 < num2 ? num1 : num2;
+}
+
+//console.log(min(2,4));
+min(2,3);
+
+// task 2
+function pow(num1, num2){
+    return Math.pow(num1, num2);
+}
+
+console.log(pow(2,4));
+
+// task 3
