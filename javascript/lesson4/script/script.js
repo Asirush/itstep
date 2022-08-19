4023f92e0bec19f1236e8be6b1042c323f441573
asirush <asirushtomblack@gmail.com>
2022-08-19 20:08:45 +0600

    tasks 2-3

--- javascript/lesson4/script/script.js
+++ javascript/lesson4/script/script.js
@@ -43,13 +43,31 @@ function getDataReturn(name){ // we can write name=Anton to set default meaning
 console.log('name: ' + getDataReturn(Anton));
 
 // ############## local variables in functions
-
 const GlobalVariable = "global variable";
 function variables(){
     const LocalVariable = "local variable";
 }
 function getVars(){
     console.log(`global = ${GlobalVariable}`);  // will be recognized
-    console.log(`local = ${LocalVariable}`);    // will be undefined
+ //   console.log(`local = ${LocalVariable}`);    // will be undefined
 }
 getVars();
+
+// ################ default values
+function defaultValues(age=0, sex=undefined){
+    console.log(`Your age: ${age}, your gender: ${sex}`);
+}
+
+defaultValues(20, "male");
+defaultValues(null, "female");
+
+function defaultValues2(age=0, name=username, sex=undefined){
+    let result = `data`;
+    if(name){result += ` name = ${name}`}
+    if(age){result += ` age = ${age}`}
+    if(sex){result += ` gender = ${sex}`}
+    console.log(result);
+}
+
+defaultValues2(20, "Sam", "male");
+defaultValues2(null, "Veronica", "female");
