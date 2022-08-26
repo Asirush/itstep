9d0a2351af06176d003c3ab56108b885d1227f85
asirush <asirushtomblack@gmail.com>
2022-08-26 21:05:13 +0600

    lesson 3 cs + homework1

---
+++ csCource/homework1/lesson2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace lesson1
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            task1();
+        }
+
+        static void task1()
+        {
+            Console.Write("input you name, surname: ");
+            string name = Console.ReadLine();
+            Console.Write("Hello, " + name);
+        }
+
+        static void task2()
+        {
+            int counter = 0, i = 0, summ = 0;
+            string[] numberList = new string[10];
+            Console.Write("input numbers: (press c to cancel)");
+            while(counter != 1)
+            {
+                string number = Console.ReadLine();
+                if (number[0] == 'c') { break; }
+                else
+                {
+                    numberList[i++] = number;
+                }
+            }
+            foreach(var item in numberList)
+            {
+                int result = Int32.Parse(item);
+                summ = summ + result;
+            }
+            Console.Write("result: " + summ);
+        }
+    }
+    
+}
---
+++ csCource/lesson3/lesson2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace lesson1
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+
+        static void task1()
+        {
+            Console.Write("input number 1: ");
+            string first_number = Console.ReadLine();
+            Console.Write("input number 2: ");
+            string second_number = Console.ReadLine();
+
+            int all = 0, all2 = 0;
+
+            foreach(var item in first_number)
+            {
+                int number = Convert.ToInt32(item.ToString());
+                all += number;
+            }
+
+            foreach (var item in second_number)
+            {
+                int number = Convert.ToInt32(item.ToString());
+                all += number;
+            }
+
+            Console.Write("Summ 1: ", all);
+            Console.Write("Summ 2: ", all2);
+        }
+        static void task2()
+        {
+            Console.Write("input number 1: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+        
+    }
+    
+}
