87ce8b0f5fdc9d826e63995c96fee1284ddafd50
asirush <asirushtomblack@gmail.com>
2022-09-09 19:20:01 +0600

    lesson 7 js

---
+++ javascript/lesson7/script/script.js
@@ -0,0 +1,18 @@
+let myString1 = "Скриптонит' - \"Цепи\" \\ \nСкриптонит' - \"Цепи\"";
+let myString2 = `Скриптонит' - "Цепи"`;
+let myString3 = 'Скриптонит\' - "Цепи"';
+
+// Example of reposition
+//let myString4 = "hello 
+//world";
+let myString5 = `Hello
+world`;
+
+document.write("<pre>");
+document.write(myString1);
+document.write(myString2);
+document.write(myString3);
+document.write("</pre>");
+console.log(myString1);
+console.log(myString2);
+console.log(myString3);
