1af36bde4daeeabc74a4188b841f877da43d53d4
asirush <asirushtomblack@gmail.com>
2022-09-09 19:39:18 +0600

    lesson 7 js end part 1

--- javascript/lesson7/script/script.js
+++ javascript/lesson7/script/script.js
@@ -1,3 +1,4 @@
+// difference of ' " and `
 let myString1 = "Скриптонит' - \"Цепи\" \\ \nСкриптонит' - \"Цепи\"";
 let myString2 = `Скриптонит' - "Цепи"`;
 let myString3 = 'Скриптонит\' - "Цепи"';
@@ -16,3 +17,20 @@ document.write("</pre>");
 console.log(myString1);
 console.log(myString2);
 console.log(myString3);
+
+// difference of string and array
+let array = ['1', '2', '3'];
+let newArray = array;
+console.log(array);
+newArray.push('4');
+console.log(array);
+console.log(newArray);
+// same with string
+let string = 'My string';
+let newString = string;
+newString += ' not changed!';
+
+// how to properly copy arrays
+let array2 = ['1', '2', '3'];
+let newArray2 = [...array2]
+
