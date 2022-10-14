e8fcb89502bb4ec7b9a88be5fbc237abd21df8e0
Asirush <asirushtomblack@gmail.com>
2022-10-14 21:42:42 +0600

    npm and node

---
+++ javascript/lesson11_npm/demo/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    function getImage(){
+        $.get(`https://picsum.photos/v2/list?page=1&limit=3`, function(responce){
+            let items = responce;
+
+            $.each()
+        })
+        getImage();
+    }
+
+    
+});
+
